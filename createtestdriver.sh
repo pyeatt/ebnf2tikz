9d1e642b39c16fd33a16909e5bc72d0e021b9a15
Dr. Larry D. Pyeatt <larry.pyeatt@sdsmt.edu>
2026-03-01 11:07:05 -0700

    Shift wrapped multi-line nodes down so rail lines align with first text line
    For nonterminals wrapped to multiple lines via \shortstack, TikZ
    anchor=west centers the node geometrically.  This caused the
    horizontal connecting line to pass between text lines instead of
    through the first line.  Shift the TikZ origin down by
    (height - singleLineHeight) / 2 while keeping entry/exit at the
    rail Y so connections align with the top line of text.
    
    Also: enable -O in make test, fix createtestdriver.sh ordering and
    format, add wide test productions exercising wrapped nodes in
    choices, loops, optionals, and nested choice contexts.

--- createtestdriver.sh
+++ createtestdriver.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+# Generate testlist.tex from test.tex newsavebox entries.
+# Must be run AFTER ebnf2tikz generates test.tex.
 
-LIST=`grep Box test.tex | grep newsave | sed -e 's/[\]newsavebox[{]//g' | sed -e 's/[}]//g'`
+LIST=$(grep newsavebox test.tex | grep Box | sed -e 's/[\]newsavebox[{]//g' | sed -e 's/[}]//g' | sort -u)
 
 rm -f testlist.tex
 
 for i in $LIST; do
-    shortname=`echo $i | sed -e 's/[\]//g'`
-    echo "\\begin{figure}" >> testlist.tex
-    echo "\\centerline{\\usebox{"$i"}}" >> testlist.tex
-    echo "\\caption{"$shortname"}"  >> testlist.tex
-    echo "\\label{fig:"$shortname"}"  >> testlist.tex
-    echo "\\end{figure}" >> testlist.tex
+    shortname=$(echo "$i" | sed -e 's/[\]//g')
+    echo "\\par\\bigskip" >> testlist.tex
+    echo "\\noindent\\textbf{${shortname}}" >> testlist.tex
+    echo "\\par\\medskip" >> testlist.tex
+    echo "\\centerline{\\usebox{${i}}}" >> testlist.tex
     echo "" >> testlist.tex
 done
