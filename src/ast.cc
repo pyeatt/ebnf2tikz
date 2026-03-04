865da990cd2e0dff8e8178fb222f191170ddb8fd
Dr. Larry D. Pyeatt <larry.pyeatt@sdsmt.edu>
2026-03-03 19:40:12 -0700

    Add ebnf2tikz LaTeX package, move sources to src/, clean up repo
    - Create LaTeX/ebnf2tikz.dtx and .ins for proper LaTeX package
      (provides all TikZ styles, lengths, macros, \useproduction,
      ebnf environment, \ebnffile, camelcase conversion, shell-escape)
    - Move all .cc/.hh/.ll/.yy source files into src/ directory
    - Convert testdriver.tex files (unit_tests, VHDL, free-pascal) to
      use \usepackage{ebnf2tikz} instead of duplicating preamble
    - Add TEXINPUTS to build scripts for finding ebnf2tikz.sty
    - Add CMake targets: sty (extract .sty), check-sty (package tests)
    - Add LaTeX/tests/ with 5 package integration tests
    - Remove documentation/latex/ (generated Doxygen output)
    - Expand realclean target to cover all generated artifacts
    - Add manual/manual.tex and write_index_redirect.cmake

---
+++ src/ast.cc
@@ -0,0 +1,170 @@
+/*
+ebnf2tikz
+    An optimizing compiler to convert (possibly annotated) Extended
+    Backus-Naur Form (EBNF) to railroad diagrams expressed as LaTeX
+    TikZ commands.
+
+    Copyright (C) 2021  Larry D. Pyeatt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file ast.cc
+ * @brief Implementation of AST node clone(), destructor, and append methods.
+ *
+ * All AST nodes support deep cloning (used during subsumption) and
+ * proper ownership-based destruction of child subtrees.
+ *
+ * @see ast.hh for the node class declarations
+ */
+
+#include "ast.hh"
+
+namespace ast {
+
+// --- clone implementations ---
+
+ASTNode* TerminalNode::clone() const
+{
+  return new TerminalNode(text);
+}
+
+ASTNode* NonterminalNode::clone() const
+{
+  return new NonterminalNode(name);
+}
+
+ASTNode* EpsilonNode::clone() const
+{
+  return new EpsilonNode();
+}
+
+ASTNode* NewlineNode::clone() const
+{
+  return new NewlineNode();
+}
+
+ASTNode* SequenceNode::clone() const
+{
+  SequenceNode *s = new SequenceNode();
+  size_t i;
+  for(i = 0; i < children.size(); i++)
+    s->append(children[i]->clone());
+  return s;
+}
+
+ASTNode* ChoiceNode::clone() const
+{
+  ChoiceNode *c = new ChoiceNode();
+  size_t i;
+  for(i = 0; i < alternatives.size(); i++)
+    c->addAlternative(alternatives[i]->clone());
+  return c;
+}
+
+ASTNode* OptionalNode::clone() const
+{
+  return new OptionalNode(child->clone());
+}
+
+ASTNode* LoopNode::clone() const
+{
+  LoopNode *n = new LoopNode();
+  size_t i;
+  if(body != NULL)
+    n->body = body->clone();
+  for(i = 0; i < repeats.size(); i++)
+    n->addRepeat(repeats[i]->clone());
+  return n;
+}
+
+// --- destructors ---
+
+SequenceNode::~SequenceNode()
+{
+  size_t i;
+  for(i = 0; i < children.size(); i++)
+    delete children[i];
+}
+
+ChoiceNode::~ChoiceNode()
+{
+  size_t i;
+  for(i = 0; i < alternatives.size(); i++)
+    delete alternatives[i];
+}
+
+OptionalNode::~OptionalNode()
+{
+  delete child;
+}
+
+LoopNode::~LoopNode()
+{
+  size_t i;
+  if(body != NULL)
+    delete body;
+  for(i = 0; i < repeats.size(); i++)
+    delete repeats[i];
+}
+
+// --- append/add methods ---
+
+void SequenceNode::append(ASTNode *child)
+{
+  children.push_back(child);
+}
+
+void ChoiceNode::addAlternative(ASTNode *alt)
+{
+  alternatives.push_back(alt);
+}
+
+void LoopNode::addRepeat(ASTNode *r)
+{
+  repeats.push_back(r);
+}
+
+} // namespace ast
+
+
+// --- ASTProduction ---
+
+ASTProduction::ASTProduction(annotmap *a, const string &n, ast::ASTNode *b)
+  : name(n), annotations(a), body(b), needsWrap(0)
+{
+}
+
+ASTProduction::~ASTProduction()
+{
+  if(annotations != NULL)
+    delete annotations;
+  if(body != NULL)
+    delete body;
+}
+
+// --- ASTGrammar ---
+
+void ASTGrammar::insert(ASTProduction *p)
+{
+  productions.push_back(p);
+}
+
+ASTGrammar::~ASTGrammar()
+{
+  size_t i;
+  for(i = 0; i < productions.size(); i++)
+    delete productions[i];
+}
---
+++ src/driver.hh
@@ -0,0 +1,152 @@
+/* 
+ebnf2tikz
+    An optimizing compiler to convert (possibly annotated) Extended
+    Backus–Naur Form (EBNF) to railroad diagrams expressed as LaTeX
+    TikZ commands.
+    
+    Copyright (C) 2021  Larry D. Pyeatt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file driver.hh
+ * @brief Parser driver class that coordinates scanning and parsing.
+ *
+ * The driver owns the output stream and node size cache references,
+ * manages Flex/Bison scanner/parser lifecycle, and tracks terminal
+ * and nonterminal symbol sets encountered during parsing.
+ *
+ * @see driver.cc for the implementation
+ * @see parser.yy for the Bison grammar that uses this driver
+ */
+
+#ifndef DRIVER_HH
+#define DRIVER_HH
+#include <set>
+#include <fstream>
+#include "ast.hh"
+#include "nodesizes.hh"
+#include "parser.hh"
+
+using namespace std;
+
+/** @brief Flex scanner function prototype, receives a driver reference. */
+#define YY_DECL yy::parser::symbol_type yylex (driver& drv)
+YY_DECL;
+
+/**
+ * @brief Coordinates scanning and parsing of EBNF input files.
+ *
+ * Acts as the interface between main(), the Flex scanner, and the
+ * Bison parser.  Holds command-line options (optimize, figures, dump)
+ * and provides factory methods for creating AST leaf nodes during parsing.
+ */
+class driver
+{
+  int result;                  /**< Parser return code. */
+  std::string file;            /**< Current input filename. */
+  set<string> nonterminals;    /**< Set of nonterminal names seen. */
+  set<string> terminals;       /**< Set of terminal strings seen. */
+  ofstream *outFile;           /**< Output file stream for TikZ. */
+  nodesizes *sizes;            /**< Node size cache from bnfnodes.dat. */
+  int noopt;                   /**< 1 to skip optimization. */
+  int figures;                 /**< 1 to wrap tikzpictures in figures. */
+  int dumponly;                /**< 1 to dump AST only (no TikZ output). */
+  int dump_before;             /**< 1 to dump AST before optimization. */
+  int dump_after;              /**< 1 to dump AST after optimization. */
+  bool trace_parsing;          /**< Enable Bison debug tracing. */
+  bool trace_scanning;         /**< Enable Flex debug tracing. */
+  yy::location location;       /**< Current source location for error reporting. */
+public:
+  /**
+   * @brief Construct a driver.
+   * @param out Output file stream for TikZ output.
+   * @param sz  Node size cache.
+   */
+  driver(ofstream *out, nodesizes *sz);
+
+  /** @brief Get the output file stream. */
+  ofstream &outs(){return *outFile;}
+
+  /** @brief Get the node size cache pointer. */
+  nodesizes *getSizes(){return sizes;}
+
+  /**
+   * @brief Create a TerminalNode and record the terminal string.
+   * @param s Terminal text (with quotes).
+   * @return Newly allocated TerminalNode.
+   */
+  ast::ASTNode* addTerminal(string &s)
+  {
+    terminals.insert(s);
+    return new ast::TerminalNode(s);
+  };
+
+  /**
+   * @brief Create a NonterminalNode and record the nonterminal name.
+   * @param s Nonterminal name.
+   * @return Newly allocated NonterminalNode.
+   */
+  ast::ASTNode* addString(string &s)
+  {
+    nonterminals.insert(s);
+    return new ast::NonterminalNode(s);
+  };
+
+  /**
+   * @brief Parse an input file.
+   * @param f       Input filename.
+   * @param opt     1 to skip optimization, 0 to optimize.
+   * @param fig     1 to wrap in figures.
+   * @param dump    1 for AST dump mode.
+   * @param dbefore 1 to dump AST before optimization.
+   * @param dafter  1 to dump AST after optimization.
+   * @return Parser return code (0 = success).
+   */
+  int parse (const char *f, int opt, int fig, int dump,
+             int dbefore, int dafter);
+
+  /** @brief Get whether dump-only mode is enabled. */
+  int get_dumponly(){return dumponly;}
+
+  /** @brief Get whether optimization is disabled. */
+  int get_noopt(){return noopt;}
+
+  /** @brief Get whether to dump AST before optimization. */
+  int get_dump_before(){return dump_before;}
+
+  /** @brief Get whether to dump AST after optimization. */
+  int get_dump_after(){return dump_after;}
+
+  /** @brief Begin scanning from the current file. */
+  void scan_begin ();
+
+  /**
+   * @brief Begin scanning from a string stream.
+   * @param s The string stream to scan.
+   */
+  void scan_begin (stringstream &s);
+
+  /** @brief End scanning and close the input. */
+  void scan_end ();
+
+  /** @brief Get the parser return code. */
+  int get_result(){return result;}
+
+  /** @brief Get a reference to the current source location. */
+  yy::location &get_location(){return location;}
+
+};
+#endif
